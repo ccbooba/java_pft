354fd3e53c49e5464d12265f6f99360b3b57695b
ccbooba <cc.booba@gmail.com>
2016-09-22 13:49:18 +0300

    Added sync.graddle configuration file

--- sandbox/src/main/java/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/stqa/pft/sandbox/Distance.java
@@ -6,18 +6,18 @@ public class Distance {
     Point point1 = new Point(5, 6);
     Point point2 = new Point(2, 2);
 
-    soutDistance(point1, point2, distance(point1, point2));
-    soutDistance(point1, point2, distance(point2, point1));
-
     soutDistance(point2, point1, point2.distance(point1));
     soutDistance(point2, point1, point1.distance(point2));
+
+/*  soutDistance(point1, point2, distance(point1, point2));
+    soutDistance(point1, point2, distance(point2, point1));*/
   }
 
-  public static double distance(Point p1, Point p2) {
+/*  public static double distance(Point p1, Point p2) {
     double delta_x = p2.x - p1.x;
     double delta_y = p2.y - p1.y;
     return Math.sqrt(delta_x * delta_x + delta_y * delta_y);
-  }
+  }*/
 
   public static void soutDistance(Point p1, Point p2, double distance) {
     System.out.println("Distance between points (" + p1.x + ", " + p1.y + ") and (" +
